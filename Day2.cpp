9d99584cbcc4fe096b9d6edd461c11e9749f23f8
starwiz-7 <54806954+starwiz-7@users.noreply.github.com>
2020-05-02 12:53:36 +0530

    Add files via upload

---
+++ Day2.cpp
@@ -0,0 +1,27 @@
+/*
+    Author: Aryan Yadav
+
+    Jewels and Stones
+*/
+using namespace std;
+class Solution
+{
+public:
+    int numJewelsInStones(string J, string S)
+    {
+        int a[256] = {0};
+        for (int i = 0; i < J.length(); i++)
+        {
+            a[J[i]]++;
+        }
+        int count = 0;
+        for (int i = 0; i < S.length(); i++)
+        {
+            if (a[S[i]] != 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
