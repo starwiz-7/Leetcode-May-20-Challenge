da037c58f8ece34c3f04c6cc7c82c871fa7f3edf
starwiz-7 <54806954+starwiz-7@users.noreply.github.com>
2020-05-02 09:45:00 +0530

    Add files via upload

---
+++ Day1.cpp
@@ -0,0 +1,26 @@
+/*
+    First Bad Version
+*/
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution
+{
+public:
+    int firstBadVersion(int n)
+    {
+        int start = 1;
+        int last = n;
+        int mid = (start + (last - start) / 2);
+        while (start <= last)
+        {
+            if (isBadVersion(mid))
+                last = mid - 1;
+            else
+                start = mid + 1;
+            mid = (start + (last - start) / 2);
+        }
+        return isBadVersion(start) ? start : start + 1;
+    }
+};
