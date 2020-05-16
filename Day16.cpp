ceef27d38028c780197ea2a3eda711ddc04addda
Aryan Yadav <54806954+starwiz-7@users.noreply.github.com>
2020-05-16 20:34:36 +0530

    Create Day16.cpp

---
+++ Day16.cpp
@@ -0,0 +1,36 @@
+/*
+    Author: Aryan Yadav
+    Odd Even Linked List
+    
+    Complexity:O(n)
+    Algorithm: NA
+    Difficulty: Medium   
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head) {
+            for (auto odd_tail = head, curr = head->next;
+                 curr && curr->next;
+                 curr = curr->next) {
+                ListNode *even_head = odd_tail->next;
+                odd_tail->next = curr->next;
+                odd_tail = odd_tail->next;
+                curr->next = odd_tail->next;
+                odd_tail->next = even_head;
+            }
+        }
+        return head; 
+    }
+};
